9b117ddde7e833dbd249380bffe579e941647cd3
Shivesh-shiva <shiveshec1032@gmail.com>
2023-09-23 14:59:22 +0530

    23/09/23

--- storage/main.tf
+++ storage/main.tf
@@ -3,8 +3,8 @@ resource "azurerm_storage_account" "storageaccount" {
   name                     = each.value.name
   resource_group_name      = each.value.resource_group_name
   location                 = each.value.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
+  account_tier             = each.value.account_tier
+  account_replication_type = each.value.account_replication_type
 
 }
 
